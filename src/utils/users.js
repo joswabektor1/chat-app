512fe92d6bc18ba2b8946a950ff862ade0d74ca2
Joshua Santiago <iamjosh004@gmail.com>
2021-09-03 15:01:31 +0800

    Initial commit

---
+++ src/utils/users.js
@@ -0,0 +1,51 @@
+const users = []
+
+const addUser = ({id, username, room}) => {
+    username = username.trim().toLowerCase()
+    room = room.trim().toLowerCase()
+
+    if (!username || !room) { 
+        return {
+            error: 'Username AND room are required!'
+        }
+    }
+    
+    const existingUser = users.find((user) => {
+        return user.room === room && user.username === username
+    })
+
+    if (existingUser) {
+        return {
+            error: 'Username is already in use!'
+        }
+    }
+
+    const user = {id, username, room} 
+    users.push(user)
+    return {user}
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id)
+    if (index !== -1) {
+        return users.splice(index, 1)[0]
+    }
+}
+
+const getUser = (id) => {
+    const index = users.find((user) => user.id === id)
+        return index
+}
+
+const getUsersInRoom = (room) => {
+    room = room.trim().toLowerCase()
+    const index = users.filter((user) => user.room === room)
+    return index
+}
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
+}
